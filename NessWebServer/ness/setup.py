a8a800c564ddea86c42d0fa1ca2790d825e9cb9a
Andre Iwers <42826581+afkiwers@users.noreply.github.com>
2025-03-14 09:46:57 +1100

    Update setup.py

--- NessWebServer/ness/setup.py
+++ NessWebServer/ness/setup.py
@@ -3,22 +3,22 @@ from ness.models import Zone
 def setup():
 
     zone_names = [
-        ["Main Floor PIRS", 1, 0x0100],
-        ["Upstairs PIR", 2, 0x0200],
-        ["Garage PIR", 3, 0x0400],
-        ["Free", 4, 0x0800],
-        ["Backyard Door", 5, 0x1000],
-        ["Main Front Door", 6, 0x2000],
-        ["Garage Back Door", 7, 0x4000],
-        ["Smoke Alarms", 8, 0x8000],
-        ["Sliding Door Balcony", 9, 0x0001],
-        ["Guest Room Window", 10, 0x0002],
-        ["Pantry Window", 11, 0x0004],
-        ["Free", 12, 0x0008],
-        ["Lower Front Door", 13, 0x0010],
-        ["Garage Roller Door", 14, 0x0020],
-        ["Free", 15, 0x0040],
-        ["Free", 16, 0x0080],
+        ["Unused", 1,  0x0100],
+        ["Unused", 2,  0x0200],
+        ["Unused", 3,  0x0400],
+        ["Unused", 4,  0x0800],
+        ["Unused", 5,  0x1000],
+        ["Unused", 6,  0x2000],
+        ["Unused", 7,  0x4000],
+        ["Unused", 8,  0x8000],
+        ["Unused", 9,  0x0001],
+        ["Unused", 10, 0x0002],
+        ["Unused", 11, 0x0004],
+        ["Unused", 12, 0x0008],
+        ["Unused", 13, 0x0010],
+        ["Unused", 14, 0x0020],
+        ["Unused", 15, 0x0040],
+        ["Unused", 16, 0x0080],
     ]
 
     for zN in zone_names:
