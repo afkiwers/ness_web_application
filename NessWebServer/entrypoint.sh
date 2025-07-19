71d82ecf27cc0586225f80b87830963d9005459a
Andre Iwers <iwers11@gmail.com>
2025-07-20 07:20:29 +1000

    added default super user creation if there are no users already

--- NessWebServer/entrypoint.sh
+++ NessWebServer/entrypoint.sh
@@ -4,6 +4,17 @@ python manage.py collectstatic --no-input
 python manage.py makemigrations
 python manage.py migrate
 
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = "${DJANGO_SUPERUSER_USERNAME:-admin}"
+email = "${DJANGO_SUPERUSER_EMAIL:-admin@example.com}"
+password = "${DJANGO_SUPERUSER_PASSWORD:-admin123}"
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+EOF
+
 gunicorn -c gunicorn.conf NessWebServer.wsgi
 
 exec "$@"
